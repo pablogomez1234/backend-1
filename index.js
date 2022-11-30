8f25e959a7b50364e7994ebbca9843eacc86ce3f
pablogomez1234 <pablogomez26995@gmail.com>
2022-11-30 14:36:59 -0300

    desafio-3gomez

--- index.js
+++ index.js
@@ -1,74 +1,24 @@
 const express = require('express');
-
-let phrase = 'Frase inicial';
-
+const Contenedor = require('./contenedor');
 const app = express();
-app.use(express.json());
+const PORT = 8080;
 
-app.delete('/api/palabras/:pos', (req, res) => {
-  const { pos } = req.params;
-
-  const phraseAsArray = phrase.split(' ');
-  const index = pos - 1;
-  const removedWord = phraseAsArray.splice(index, 1)[0];
-
-  res.json({
-    removida: removedWord,
-  });
+const server = app.listen(process.env.PORT || PORT, () => {
+  console.log(`server listening on PORT ${PORT}`);
 });
 
-app.put('/api/palabras/:pos', (req, res) => {
-  const { pos } = req.params;
-  const { palabra } = req.body;
+server.on('error', err => console.log(`error: ${err}`));
 
-  const phraseAsArray = phrase.split(' ');
-  const index = pos - 1;
-  const anterior = phraseAsArray[index];
-  phraseAsArray[index] = palabra;
+const products = new Contenedor('products.txt'),
 
-  phrase = phraseAsArray.join(' ');
-
-  res.json({
-    actualizada: palabra,
-    anterior,
-  });
+app.get('/productos', async (req, res) => {
+  const productos = await products.getAll();
+  res.send(productos);
 });
 
-app.get('/api/frase', (req, res) => {
-  res.json({ frase: phrase });
+app.get('/productoRandom', async (req, res) => {
+  const productos = await products.getAll();
+  let numeroRandom = Match.floor(Match.random() = productos.length);
+  res.send(productos[numeroRandom]);
 });
-
-app.get('/api/palabras/:pos', (req, res) => {
-  const phraseAsArray = phrase.split(' ');
-  const { pos } = req.params;
-  const index = pos - 1;
-
-  res.json({ buscada: phraseAsArray[index] });
-});
-
-app.post('/api/palabras', (req, res) => {
-  console.log(req.body)
-  const { palabra } = req.body;
-
-  const phraseAsArray = phrase.split(' ');
-  phraseAsArray.push(palabra);
-
-  phrase = phraseAsArray.join(' ');
-
-  res.json({
-    agregada: palabra,
-    pos: phraseAsArray.length,
-    frase: phrase,
-  })
-});
-
-app.listen(8080, () => console.log(`I'm here!`));
-
-
-
-
-
-
-
-
-
+ 
